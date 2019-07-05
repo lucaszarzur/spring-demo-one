6e9061bdd26cf00e8dfa324006e2161c2516cc3c
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-05 00:50:26 -0300

    Spring Dependency Injection 1

--- src/com/lucas/springdemo/TrackCoach.java
+++ src/com/lucas/springdemo/TrackCoach.java
@@ -8,4 +8,9 @@ public class TrackCoach implements Coach {
     public String getDailyWorkout() {
         return "Run a hard 5k";
     }
+
+    @Override
+    public String getDailyFortune() {
+        return null;
+    }
 }
---
+++ src/com/lucas/springdemo/service/FortuneService.java
@@ -0,0 +1,6 @@
+package com.lucas.springdemo.service;
+
+public interface FortuneService {
+
+    public String getFortune();
+}
