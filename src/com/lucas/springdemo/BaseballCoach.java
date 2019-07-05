c820562114560f4469dad972e5a230a5195c7016
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-05 00:54:57 -0300

    Spring Dependency Injection 2

--- src/com/lucas/springdemo/BaseballCoach.java
+++ src/com/lucas/springdemo/BaseballCoach.java
@@ -1,16 +1,27 @@
 package com.lucas.springdemo;
 
 import com.lucas.springdemo.interfaces.Coach;
+import com.lucas.springdemo.service.FortuneService;
 
 public class BaseballCoach implements Coach {
 
+    // define a private field for the dependecy
+    private FortuneService fortuneService;
+
+    //define a constructor for dependecy injection
+    public BaseballCoach(FortuneService theFortuneService) {
+        fortuneService = theFortuneService;
+    }
+
     @Override
-    public String getDailyWorkout() {
+    public String getDailyWorkout(){
         return "Spend 30 minutes on batting practice";
     }
 
     @Override
     public String getDailyFortune() {
-        return null;
+
+        // use my fortuneService to get a fortune
+        return fortuneService.getFortune();
     }
 }
