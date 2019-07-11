a7b38fdcb15754184418d12d0cde0b664a4f7f92
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-11 00:01:12 -0300

    1.1 - Beans Scopes - Singleton

--- src/com/lucas/springdemo/BeanScopeDemoApp.java
+++ src/com/lucas/springdemo/BeanScopeDemoApp.java
@@ -13,5 +13,17 @@ public class BeanScopeDemoApp {
         Coach theCoach = context.getBean("myCoach", Coach.class);
 
         Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (theCoach == alphaCoach);
+
+        // print out the results
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+        System.out.println("\nMemory location for alphaCoach: " + alphaCoach);
+
+        // close the context
+        context.close();
     }
 }
