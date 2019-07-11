0259f175d8e76c643b8ab80a6e3feb3c1005f1a5
Lucas Zarzur <lucas.teles@fh.com.br>
2019-07-10 23:55:11 -0300

    1 - Beans Scopes - Singleton

---
+++ src/com/lucas/springdemo/BeanScopeDemoApp.java
@@ -0,0 +1,17 @@
+package com.lucas.springdemo;
+
+import com.lucas.springdemo.interfaces.Coach;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+    public static void main(String[] args) {
+
+        // load the spring configuration file
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+
+        // retrieve bean from spring container
+        Coach theCoach = context.getBean("myCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("myCoach", Coach.class);
+    }
+}
